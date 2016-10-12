3a46af147761262c69b3168752d6be9adc296bb5
Santeri Hetekivi <santeri@hetekivi.com>
2016-10-12 10:00:59 +0300

    Installing hadoop

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
-# Hadoop user
+# Install java
+
+# Adding Hadoop user
 sudo mkdir -p /user/hadoop
 sudo useradd hadoop -d /user/hadoop hadoop
 sudo chmod 700 /user/hadoop
 sudo chown hadoop:hadoop /user/hadoop
 
-# Downloading hadoop
+# Downloading Hadoop
 sudo wget http://apache.claz.org/hadoop/common/hadoop-2.7.2/hadoop-2.7.2.tar.gz -P /usr/local/
 sudo tar xzf /usr/local/hadoop-2.7.2.tar.gz -C /usr/local
-mv /usr/local/hadoop-2.7.2 /usr/local/hadoop
+sudo mv /usr/local/hadoop-2.7.2 /usr/local/hadoop
+
+# Installing Hadoop
+echo 'export HADOOP_HOME=/usr/local/hadoop' >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/hadoop/bin' >> ~/.bashrc
+# echo 'export JAVA_HOME=/usr/lib/jvm/java-8-jdk' >> ~/.bashrc # Replace with your own jdk path
