dcfeba9468f77b4053e28eebac7af08a24bd37cf
Santeri Hetekivi <santeri@hetekivi.com>
2016-10-12 09:34:26 +0300

    Downloading hadoop

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ sudo mkdir -p /user/hadoop
 sudo useradd hadoop -d /user/hadoop hadoop
 sudo chmod 700 /user/hadoop
 sudo chown hadoop:hadoop /user/hadoop
+
+# Downloading hadoop
+sudo wget http://apache.claz.org/hadoop/common/hadoop-2.7.2/hadoop-2.7.2.tar.gz -P /usr/local/
+sudo tar xzf /usr/local/hadoop-2.7.2.tar.gz -C /usr/local
+mv /usr/local/hadoop-2.7.2 /usr/local/hadoop
