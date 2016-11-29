8ca930e3e0fdae26695dff7ee3c365227a6c9815
Santeri Hetekivi <santeri@hetekivi.com>
2016-11-29 13:26:10 +0200

    Changed to csv sample data

--- electrical_consumption/run.sh
+++ electrical_consumption/run.sh
@@ -4,6 +4,7 @@ WORKING_DIR="/user/hadoop/electrical_consumption"
 INPUT_DIR="${WORKING_DIR}/input_dir"
 OUTPUT_DIR="${WORKING_DIR}/output_dir"
 UNITS_DIR="${WORKING_DIR}/units"
+SAMPLE_FILENAME="sample.csv"
 
 rm -rf ${WORKING_DIR}
 mkdir -p ${UNITS_DIR}
@@ -11,7 +12,7 @@ cp ${DIR}/to_user_dir/* ${WORKING_DIR}/
 javac -classpath ${WORKING_DIR}/hadoop-core.jar -d ${UNITS_DIR} ${WORKING_DIR}/ProcessUnits.java
 jar -cvf ${WORKING_DIR}/units.jar -C ${UNITS_DIR}/ .
 ${HADOOP_HOME}/bin/hadoop fs -mkdir ${INPUT_DIR}
-${HADOOP_HOME}/bin/hadoop fs -put ${WORKING_DIR}/sample.txt ${INPUT_DIR}
+${HADOOP_HOME}/bin/hadoop fs -put ${WORKING_DIR}/${SAMPLE_FILENAME} ${INPUT_DIR}
 ${HADOOP_HOME}/bin/hadoop fs -ls ${INPUT_DIR}/
 ${HADOOP_HOME}/bin/hadoop jar ${WORKING_DIR}/units.jar hadoop.ProcessUnits ${INPUT_DIR} ${OUTPUT_DIR}
 ${HADOOP_HOME}/bin/hadoop fs -ls ${OUTPUT_DIR}/
