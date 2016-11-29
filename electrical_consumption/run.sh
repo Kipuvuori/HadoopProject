a2d5ed9862ffa8808d7188489e1d687a8b1d3adc
Santeri Hetekivi <santeri@hetekivi.com>
2016-11-29 13:16:49 +0200

    Changed jar command to use right directories

--- electrical_consumption/run.sh
+++ electrical_consumption/run.sh
@@ -9,7 +9,7 @@ rm -rf ${WORKING_DIR}
 mkdir -p ${UNITS_DIR}
 cp ${DIR}/to_user_dir/* ${WORKING_DIR}/
 javac -classpath ${WORKING_DIR}/hadoop-core.jar -d ${UNITS_DIR} ${WORKING_DIR}/ProcessUnits.java
-jar -cvf units.jar -C ${UNITS_DIR}/ ${WORKING_DIR}/
+jar -cvf ${WORKING_DIR}/units.jar -C ${UNITS_DIR}/ .
 ${HADOOP_HOME}/bin/hadoop fs -mkdir ${INPUT_DIR}
 ${HADOOP_HOME}/bin/hadoop fs -put ${WORKING_DIR}/sample.txt ${INPUT_DIR}
 ${HADOOP_HOME}/bin/hadoop fs -ls ${INPUT_DIR}/
