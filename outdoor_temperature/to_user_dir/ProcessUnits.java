82882166b9826d5840d59ffbc98d7e3d38a39f6d
Anton Lehmus <anton.lehmus@gmail.com>
2016-12-01 13:31:10 +0200

    test max teperature example

--- outdoor_temperature/to_user_dir/ProcessUnits.java
+++ outdoor_temperature/to_user_dir/ProcessUnits.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class ProcessUnits {
     //Mapper class
