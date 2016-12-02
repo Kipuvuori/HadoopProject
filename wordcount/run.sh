ef7eec16ec33b05aa3da095449e32c89ef2b424a
Santeri Hetekivi <SanteriHetekivi@users.noreply.github.com>
2016-12-02 15:49:56 +0200

    2) Electrical consumption (#1)
    * First version of install script.
    
    * Changed file ending
    
    * Changed file ending
    
    * Removed user change
    
    * Normalized hadoop-core
    
    * Making universal
    
    * Adding removing
    
    * Added dir for units
    
    * Changed jar command to use right directories
    
    * Changed right path for units.jar
    
    * Changed to csv sample data
    
    * Added run output
    
    * Added checks and comments to run.sh code.
    
    * Removed files that runing the code made.
    
    * Made run script for wordcount
    
    * Changed wordcount's run.sh to LF
    
    * Fixed DIR and added confirmation step.
    
    * Changed workcount to runnable
    
    * Changed electrical_consumption to runnable
    
    * Electrocal consumption running files

---
+++ wordcount/run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -z "$1" ]
+# If first argument is empty.
+then
+    # Set default working directory.
+    WORKING_DIR=${DIR}
+# If first argument is given
+else
+    # Set given working directory
+    WORKING_DIR="${1%\/}"
+fi
+
+echo "Working directory: ${WORKING_DIR}"
+
+# Ask user if he wants to continue with current settings.
+read -p "Do you want to continue? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+# Setting directories to use.
+INPUT_DIR="${WORKING_DIR}/input"
+OUTPUT_DIR="${WORKING_DIR}/output"
+
+# Removing input and output directories.
+rm -rf ${INPUT_DIR}
+rm -rf ${OUTPUT_DIR}
+# Making input directory.
+mkdir -p ${INPUT_DIR}
+# Adding input files to input directory.
+cp ${DIR}/input_files/* ${INPUT_DIR}/
+# Run wordcount program on input dir and output results to output dir.
+hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.2.jar wordcount ${INPUT_DIR} ${OUTPUT_DIR}
