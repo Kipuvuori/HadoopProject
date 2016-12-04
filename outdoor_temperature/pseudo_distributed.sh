b41bf4521aa677bb8b48862e56604f38d1152755
Anton Lehmus <anton.lehmus@gmail.com>
2016-12-04 14:16:09 +0200

    Outdoor temperature (#3)
    * First version of install script.
    
    * Changed file ending
    
    * Changed file ending
    
    * Removed user change
    
    * Normalized hadoop-core
    
    * Making universal
    
    * Adding removing
    
    * Added dir for units
    
    * Changed jar command to use right directories
    
    * Changed right path for units.jar
    
    * Changed to csv sample data
    
    * Added checks and comments to run.sh code.
    
    * Removed files that runing the code made.
    
    * Made run script for wordcount
    
    * Changed wordcount's run.sh to LF
    
    * Fixed DIR and added confirmation step.
    
    * Changed workcount to runnable
    
    * Changed electrical_consumption to runnable
    
    * base outdoor temp of electrical consumption. Modify run.sh to suit pseudo distibuted mode
    
    * add sample file. fiddlle around with ProcessUnits.java
    
    * add simple (y/n) to indicate valdit input to run scripts
    
    * move csv donwload to more sensible palce on run script
    
    * update run script
    
    * update run script
    
    * make cp verbose and recursive
    
    * update run.sh
    
    * echo temp.csv download location
    
    * move code changes to corret java file
    
    * remove extra java includes
    
    * re copy ProcessUnits
    
    * add mkdir for output and input directories
    
    * add parent direcotry creation to hadoopFS mkdir
    
    * test max teperature example
    
    * test max teperature example
    
    * test max teperature example
    
    * test max teperature example
    
    * test max teperature example
    
    * test max teperature example
    
    * make expable use double isntead of int
    
    * test max teperature example
    
    * convert max temperature redurer to calculate averate temperature
    
    * add rounding to temperatures
    
    * calculate avg temperature for each day
    
    * fix date parsing
    
    * add arguments for mapreduce job
    
    * fix reading arguments
    
    * fix reading arguments
    
    * update reading arguments on run.sh
    
    * update reading arguments on java
    
    * update reading arguments on java
    
    * check for empty key
    
    * check for empty key
    
    * add debug output
    
    * split csv also by line change
    
    * check if key is space
    
    * add run excample outputs
    
    * Fixed -6.2 error by trimming empty chars away
    
    * update run output examples

---
+++ outdoor_temperature/pseudo_distributed.sh
@@ -0,0 +1,9 @@
+# Installing Hadoop
+echo 'export HADOOP_MAPRED_HOME=$HADOOP_HOME' >> ~/.bashrc
+echo 'export HADOOP_COMMON_HOME=$HADOOP_HOME' >> ~/.bashrc
+echo 'export HADOOP_HDFS_HOME=$HADOOP_HOME' >> ~/.bashrc
+echo 'export YARN_HOME=$HADOOP_HOME' >> ~/.bashrc
+echo 'export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native' >> ~/.bashrc
+echo 'export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin' >> ~/.bashrc
+echo 'export HADOOP_INSTALL=$HADOOP_HOME' >> ~/.bashrc
+source ~/.bashrc 
---
+++ outdoor_temperature/to_user_dir/ProcessUnits.java
@@ -0,0 +1,105 @@
+package hadoop;
+
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+
+public class ProcessUnits {
+    //Mapper class
+    public static class TempMap extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+
+        public void map(LongWritable key, Text value, Context context)
+                throws IOException, InterruptedException {
+
+            Configuration conf = context.getConfiguration();
+            String param = conf.get("time_unit");
+
+            int date_parse_end = 0;
+
+            switch(param.charAt(0)) {
+                case 'y' :
+                    date_parse_end = 4;
+                    break;
+
+                case 'm' :
+                    date_parse_end = 7;
+                    break;
+
+                default :
+                    date_parse_end = 10;
+            }
+
+            String record = value.toString();
+            //split csv
+            String[] parts = record.split(",");
+            String date_time = parts[0].trim().substring(0,date_parse_end);
+            double temp = Double.parseDouble(parts[1]);
+            //extract date and temperature
+            context.write(new Text(date_time), new DoubleWritable(temp));
+        }
+    }
+
+
+    //Reducer class
+    public static class TempReduce extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+
+        public void reduce(Text key, Iterable<DoubleWritable> values,Context context) throws IOException, InterruptedException {
+
+            double temp_sum = 0;
+            int item_sum = 0;
+
+            //Looping and calculating average for each time unit
+            for (DoubleWritable val : values) {
+                temp_sum += val.get();
+                item_sum++;
+            }
+
+            //write key and value using 2 decimals
+            context.write(key, new DoubleWritable(Math.round((temp_sum/item_sum)*100.0)/100.0));
+        }
+    }
+
+
+
+
+    //Main function
+    public static void main(String args[]) throws Exception {
+        Configuration conf = new Configuration();
+
+        conf.set("time_unit", args[2]);
+
+        Job job = new Job(conf, "avg_temp");
+
+        job.setJarByClass(ProcessUnits.class);
+
+        job.setMapOutputKeyClass(Text.class);
+
+        job.setMapOutputValueClass(DoubleWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+
+        job.setOutputValueClass(DoubleWritable.class);
+
+        job.setMapperClass(TempMap.class);
+
+        job.setReducerClass(TempReduce.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
+}
